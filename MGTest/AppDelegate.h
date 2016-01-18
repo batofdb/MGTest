8e379ea0f24a5c5f7ffa19eb976c3e6859a15535
Francis Bato <batofdb@gmail.com>
2016-01-17 22:07:13 -0800

    Initial Commit

---
+++ MGTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MGTest
+//
+//  Created by Francis Bato on 1/17/16.
+//  Copyright © 2016 FrancisBato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MGTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MGTest
+//
+//  Created by Francis Bato on 1/17/16.
+//  Copyright © 2016 FrancisBato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
